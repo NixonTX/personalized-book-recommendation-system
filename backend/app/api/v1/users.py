c75106d9b6175e06c3f7d9bf06446dcc8257b248
NixonTX <nixontx321@gmail.com>
2025-04-13 13:09:31 +0530

    'users/me ' endpoint

--- backend/app/api/v1/users.py
+++ backend/app/api/v1/users.py
@@ -6,7 +6,7 @@ from backend.app.schemas.user import UserCreate, UserResponse
 from backend.app.services.users import get_user_by_email, create_user
 from backend.app.database.db import get_db
 
-router = APIRouter()
+router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/users/", response_model=UserResponse, status_code=201)
 async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
@@ -16,3 +16,7 @@ async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     
     created_user = await create_user(db, user)  # Pass UserCreate object directly
     return UserResponse.model_validate(created_user)
+
+@router.get("/me", response_model=UserResponse)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
